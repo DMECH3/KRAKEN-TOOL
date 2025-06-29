022ca0642ad26ea7351fc9d9e6fd11ed164972e8
VSSVINTOREZ <87673425+DMECH3@users.noreply.github.com>
2025-06-29 19:19:47 -0400

    Update Ping.py
    changed color

--- scripts/Ping.py
+++ scripts/Ping.py
@@ -1,8 +1,11 @@
-import os
+import subprocess
 from pystyle import Colorate, Colors
 
 ip = " Input the ip -> "
 
-
-os.system(f'Ping '+ ip )
+Ip_Color = Colorate.Horizontal(Colors.blue_to_purple, ip)
+ip = input(Ip_Color)
+subprocess.run(f"ping " + ip)
 input("press enter to complete...")
+
+#Cyssym
